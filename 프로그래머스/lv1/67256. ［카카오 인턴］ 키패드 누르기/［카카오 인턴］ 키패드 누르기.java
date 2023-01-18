37a8634c929bbaa0f07c8178e6c0887458958d25
JWY <107178253+jwyong4@users.noreply.github.com>
2023-01-18 17:16:31 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 3.39 ms, Memory: 69.7 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,8 +1,8 @@
 class Solution {
     public String solution(int[] numbers, String hand) {
         String answer = "";
-        int rHand = 12;
-        int lHand = 10;
+        int rHand = 12; // 오른손 시작위치 #
+        int lHand = 10; // 왼손 시작위치 *
         int[][] phone = {
             {1, 2, 3},
             {4, 5, 6},
@@ -11,31 +11,39 @@ class Solution {
         };
         
         for (int i = 0; i < numbers.length; i++) {
+            // number가 0이면 11로 변경
             if (numbers[i] == 0) {
                 numbers[i] = 11;
             }
             
             for (int j = 0; j < phone.length; j++) {
+                // number의 y 좌표값이 0이면 왼손
                 if (numbers[i] == phone[j][0]) {
                     lHand = numbers[i];
                     answer += "L";
                     break;
                     
+                // number의 y 좌표값이 2이면 오른손
                 } else if (numbers[i] == phone[j][2]) {
                     rHand = numbers[i];
                     answer += "R";
                     break;
                     
+                // number의 y 좌표값이 1이면 비교 시작
                 } else if (numbers[i] == phone[j][1]) {
+                    // 각 손의 현재 좌표값 구하기
                     int[] indexL = xy(lHand, phone);
                     int[] indexR = xy(rHand, phone);
+                    // 각 손의 좌표값과 number의 좌표값을 빼주어 거리값 계산
+                    int lDistance = Math.abs(indexL[0] - j) + Math.abs(indexL[1] - 1);
+                    int rDistance = Math.abs(indexR[0] - j) + Math.abs(indexR[1] - 1);
                     
-                    if (Math.abs(indexL[0] - j) + Math.abs(indexL[1] - 1) < Math.abs(indexR[0] - j) + Math.abs(indexR[1] - 1)) {
+                    if (lDistance < rDistance) {
                         lHand = numbers[i];
                         answer += "L";
                         break;
                         
-                    } else if (Math.abs(indexL[0] - j) + Math.abs(indexL[1] - 1) > Math.abs(indexR[0] - j) + Math.abs(indexR[1] - 1)) {
+                    } else if (lDistance > rDistance) {
                         rHand = numbers[i];
                         answer += "R";
                         break;
@@ -61,6 +69,7 @@ class Solution {
         return answer;
     }
     
+    // phone을 순회하며 입력받은 num의 좌표값을 찾아서 배열로 리턴
     public int[] xy(int num, int[][] phone) {
         for (int i = 0; i < phone.length; i++) {
             for (int j = 0; j < phone[i].length; j++) {
