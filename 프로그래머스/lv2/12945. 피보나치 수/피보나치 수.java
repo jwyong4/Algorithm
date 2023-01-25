a65c1d0794d1b36b30a6a63b86aa1800c689d304
JWY <107178253+jwyong4@users.noreply.github.com>
2023-01-25 10:36:31 +0900

    [level 2] Title: 피보나치 수, Time: 13.33 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
@@ -1,5 +1,6 @@
 class Solution {
     static int[] arr = new int[100001];
+    
     public int solution(int n) {
         int answer = fibo(n);
         return answer;
@@ -8,8 +9,10 @@ class Solution {
     public int fibo(int num) {
         if (num <= 1) {
             return num;
+            
         } else if (arr[num] != 0) {
             return arr[num];
+            
         } else {
             return arr[num] = (fibo(num - 1) + fibo(num - 2)) % 1234567;
         }
