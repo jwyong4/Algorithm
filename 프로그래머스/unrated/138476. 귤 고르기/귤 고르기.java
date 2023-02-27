52ddc62fd510dd6ca1c2c07dc38590c2f46416a0
JWY <107178253+jwyong4@users.noreply.github.com>
2023-02-27 17:03:23 +0900

    [unrated] Title: 귤 고르기, Time: 49.93 ms, Memory: 91.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < tangerine.length; i++) {
+            map.put(tangerine[i], map.getOrDefault(tangerine[i], 0) + 1);
+        }
+        
+        List<Map.Entry<Integer, Integer>> entryList = new ArrayList<>(map.entrySet());
+        entryList.sort(new Comparator<Map.Entry<Integer, Integer>>() {
+            @Override
+            public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
+                return o2.getValue().compareTo(o1.getValue());
+            }
+        });
+        
+        for (Map.Entry<Integer, Integer> entry : entryList) {
+            if (k <= 0) {
+                break;
+            } else {
+                k -= entry.getValue();
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
